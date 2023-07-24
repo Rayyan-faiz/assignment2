8ef75fdb572a5978d978d73fc09841a7a18de7e1
Rayyan-faiz <rayyanfaiz2000@gmail.com>
2023-07-24 23:27:30 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,49 @@
+//task:1
+let message = 'Hello, World';
 
-let num1 = 1000;
-let num2 = 1200;
-let num3 = num1 /  num2
-console.log (num3);
+console.log(message);
+
+//task:2
+
+let quote = "A person who never made a mistake never tried anything new.";
+
+let author = "Albert Einstein";
+
+console.log(`${author} once said, "${quote}"`);
+
+//task:3
+
+ 
+let personNameWithWhitespace = "\t   ibrahim\n   \t";
+
+console.log(`Name with whitespaces: "${personNameWithWhitespace}"`);
+
+
+let personNameStripped = personNameWithWhitespace.trim();
+
+console.log(`Name without whitespaces: "${personNameStripped}"`);
+
+//task:4
+
+let num1 = 10;
+
+let num2 = 2000;
+
+let num3 = num1 + num2;
+
+console.log(num3);
+
+//task:5
+
+let numberA= 200;
+
+let numberB=100;
+
+let numberC=numberA - numberB;
+
+console.log  (numberC);
+
+
+//task:6
+
+
